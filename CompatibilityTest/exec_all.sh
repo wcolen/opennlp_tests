34eca0fcc70e448d096578dca971ea2f06513895
William Colen <william.colen@gmail.com>
2015-01-28 10:56:15 -0200

    removing duplicated

--- CompatibilityTest/exec_all.sh
+++ CompatibilityTest/exec_all.sh
@@ -2,7 +2,7 @@
 
 mkdir target/res
 
-for i in "1.5.2-incubating" "1.5.2-incubating" "1.5.3" "1.5.3" "1.6.0" "1.6.0"
+for i in "1.5.2-incubating" "1.5.3" "1.6.0"
 do	
 	echo Testing ${i}
 	time sh exec.sh ${i} > target/res/allout_${i}.txt 2>&1
